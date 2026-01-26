7764c81ff7163686c4b3a5620cb3e468a29e9f46
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-01-26 16:46:19 +0530

    frontend initial

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("contactForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  const status = document.getElementById("statusMessage");
+
+  const response = await fetch("https://portfolio-backend-ay54.onrender.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, message }),
+  });
+
+  const data = await response.json();
+
+  // ✅ SUCCESS MESSAGE
+  status.style.color = "green";
+  status.innerText = "✅ Message sent successfully!";
+
+  // clear form
+  document.getElementById("contactForm").reset();
+});
