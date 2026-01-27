20453a123ba47c79ba696a6397f442eaa3d992f2
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-01-27 20:19:02 +0530

    some change

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
   const message = document.getElementById("message").value;
   const status = document.getElementById("statusMessage");
 
-  const response = await fetch("https://portfolio-backend-ay54.onrender.com/api/contact", {
+  const response = await fetch("https://portfolio-backend-7zcq.onrender.com/api/contact", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
